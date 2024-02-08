8297121a4e794218342b3784faaaa6deab738a9a
carson2222 <olcraftyt@gmail.com>
2024-02-08 15:00:41 +0100

    12-18

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,14 @@
-export default function ProductDetails({ params }: { params: { productId: string } }) {
+import { Metadata } from "next";
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Product ${params.productId}`,
+  };
+};
+
+export default function ProductDetails({ params }: Props) {
   return <h1>Details product description {params.productId}</h1>;
 }
+type Props = {
+  params: { productId: string };
+};
